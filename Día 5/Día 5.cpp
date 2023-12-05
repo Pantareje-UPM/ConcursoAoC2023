a939b59b812c64bac10ebe24c6d218b7a07081ac
Pantareje <62074882+Pantareje@users.noreply.github.com>
2023-12-05 15:36:02 +0100

    Día 5 parte 2.

--- Día 5/Día 5.cpp
+++ Día 5/Día 5.cpp
@@ -1,8 +1,11 @@
+#include <algorithm>
+#include <vector>
 #include <array>
 #include <set>
 #include <cassert>
 #include <fstream>
 #include <iostream>
+#include <optional>
 
 struct Node
 {
@@ -10,14 +13,14 @@ struct Node
     uint32_t fromStart;
     uint32_t length;
 
-    [[nodiscard]] bool IsBefore(const uint32_t a) const noexcept
+    [[nodiscard]] bool IsBeforeOrEqual(const uint32_t a) const noexcept
     {
         return fromStart <= a;
     }
 
     [[nodiscard]] bool Contains(const uint32_t a) const noexcept
     {
-        assert(IsBefore(a));
+        assert(IsBeforeOrEqual(a));
         return a - fromStart < length;
     }
 
@@ -33,33 +36,57 @@ struct Node
     }
 };
 
+template<typename T>
+struct Range
+{
+    T start;
+    T length;
+
+    friend bool operator<(const Range& a, const Range& b) noexcept
+    {
+        return a.start < b.start;
+    }
+
+    friend bool operator>(const Range& a, const Range& b) noexcept
+    {
+        return a.start > b.start;
+    }
+};
+
 struct Almanac
 {
-    std::set<uint32_t> seeds;
+    std::vector<uint32_t> seeds;
+
+    std::vector<Range<uint32_t>> seedRanges;
     std::array<std::set<Node>, 7> maps;
 };
 
 uint32_t First(const Almanac& almanac)
 {
-    std::set<uint32_t> setA(almanac.seeds);
-    std::set<uint32_t> setB;
+    std::vector<uint32_t> setA(almanac.seeds);
+    std::vector<uint32_t> setB;
 
-    std::set<uint32_t>* from = &setA;
-    std::set<uint32_t>* to = &setB;
+    std::vector<uint32_t>* from = &setA;
+    std::vector<uint32_t>* to = &setB;
 
     for (const auto& map : almanac.maps)
     {
-        auto seed = from->begin();
         auto node = map.begin();
+        std::optional seed = std::make_optional(from->front());
 
-        while (seed != from->end() && node != map.end())
+        while (seed.has_value() && node != map.end())
         {
-            if (node->IsBefore(*seed))
+            if (node->IsBeforeOrEqual(*seed))
             {
                 if (node->Contains(*seed))
                 {
-                    to->emplace(node->Translate(*seed));
-                    ++seed;
+                    to->emplace_back(node->Translate(*seed));
+                    std::ranges::push_heap(*to, std::greater{});
+
+                    std::ranges::pop_heap(*from, std::greater{});
+                    from->resize(from->size() - 1);
+
+                    seed = !from->empty() ? std::make_optional(from->front()) : std::nullopt;
                 }
                 else
                 {
@@ -68,25 +95,126 @@ uint32_t First(const Almanac& almanac)
             }
             else
             {
-                to->emplace(*seed);
-                ++seed;
+                to->emplace_back(*seed);
+                std::ranges::push_heap(*to, std::greater{});
+
+                std::ranges::pop_heap(*from, std::greater{});
+                from->resize(from->size() - 1);
+
+                seed = !from->empty() ? std::make_optional(from->front()) : std::nullopt;
             }
         }
 
-        while (seed != from->end())
+        while (seed.has_value())
         {
-            to->emplace(*seed);
-            ++seed;
+            to->emplace_back(*seed);
+            std::ranges::push_heap(*to, std::greater{});
+
+            std::ranges::pop_heap(*from, std::greater{});
+            from->resize(from->size() - 1);
+
+            seed = !from->empty() ? std::make_optional(from->front()) : std::nullopt;
         }
 
-        from->clear();
-
-        std::set<uint32_t>* tmp = from;
+        std::vector<uint32_t>* tmp = from;
         from = to;
         to = tmp;
     }
 
-    return *from->begin();
+    return from->front();
+}
+
+uint32_t Second(const Almanac& almanac)
+{
+    using RangeHeap = std::vector<Range<uint32_t>>;
+
+    RangeHeap setA(almanac.seedRanges);
+    RangeHeap setB;
+
+    RangeHeap* from = &setA;
+    RangeHeap* to = &setB;
+
+    for (const auto& map : almanac.maps)
+    {
+        auto node = map.begin();
+        std::optional seedRange = std::make_optional(from->front());
+
+        while (seedRange.has_value() && node != map.end())
+        {
+            const auto seedStart = seedRange->start;
+            const auto nodeStart = node->fromStart;
+
+            const auto seedEnd = seedRange->start + seedRange->length;
+            const auto nodeEnd = node->fromStart + node->length;
+
+            if (nodeStart < seedEnd)
+            {
+                if (seedStart < nodeEnd)
+                {
+                    if (seedStart < nodeStart)
+                    {
+                        to->emplace_back(seedStart, nodeStart - seedStart);
+                        std::ranges::push_heap(*to, std::greater{});
+                    }
+
+                    const auto firstToTranslate = std::max(nodeStart, seedStart);
+                    const auto lastToTranslate = std::min(nodeEnd, seedEnd);
+
+                    to->emplace_back(node->Translate(firstToTranslate), lastToTranslate - firstToTranslate);
+                    std::ranges::push_heap(*to, std::greater{});
+
+                    if (seedEnd > nodeEnd)
+                    {
+                        std::ranges::pop_heap(*from, std::greater{});
+                        from->resize(from->size() - 1);
+
+                        from->emplace_back(nodeEnd, seedEnd - nodeEnd);
+                        std::ranges::push_heap(*from, std::greater{});
+
+                        seedRange = !from->empty() ? std::make_optional(from->front()) : std::nullopt;
+                    }
+                    else
+                    {
+                        std::ranges::pop_heap(*from, std::greater{});
+                        from->resize(from->size() - 1);
+
+                        seedRange = !from->empty() ? std::make_optional(from->front()) : std::nullopt;
+                    }
+                }
+                else
+                {
+                    ++node;
+                }
+            }
+            else
+            {
+                to->emplace_back(*seedRange);
+                std::ranges::push_heap(*to, std::greater{});
+
+                std::ranges::pop_heap(*from, std::greater{});
+                from->resize(from->size() - 1);
+
+                seedRange = !from->empty() ? std::make_optional(from->front()) : std::nullopt;
+            }
+        }
+
+        while (seedRange.has_value())
+        {
+            to->emplace_back(*seedRange);
+            std::ranges::push_heap(*to, std::greater{});
+
+            std::ranges::pop_heap(*from, std::greater{});
+            from->resize(from->size() - 1);
+
+            seedRange = !from->empty() ? std::make_optional(from->front()) : std::nullopt;
+        }
+
+        RangeHeap* tmp = from;
+        from = to;
+        to = tmp;
+    }
+
+    return from->front().start;
 }
 
 int main()
@@ -98,10 +226,17 @@ int main()
     input.ignore(7);
     while (std::isdigit(input.peek()))
     {
-        uint32_t seedNumber;
-        input >> seedNumber >> std::ws;
-        almanac.seeds.emplace(seedNumber);
+        uint32_t seedStart, seedLength;
+        input >> seedStart >> seedLength >> std::ws;
+
+        almanac.seeds.emplace_back(seedStart);
+        almanac.seeds.emplace_back(seedLength);
+
+        almanac.seedRanges.emplace_back(seedStart, seedLength);
     }
+
+    std::ranges::make_heap(almanac.seeds, std::greater{});
+    std::ranges::make_heap(almanac.seedRanges, std::greater{});
 
     for (auto& map : almanac.maps)
     {
@@ -115,4 +250,5 @@ int main()
     }
 
     std::cout << First(almanac) << std::endl;
+    std::cout << Second(almanac) << std::endl;
 }
